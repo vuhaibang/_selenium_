25a70bc751d3cd39e8463c2228d4a6824426585d
bang vu hai <bangvh@vnfintech.com>
2020-07-16 21:20:34 +0700

    ok

--- app.py
+++ app.py
@@ -6,11 +6,26 @@ import random
 display = Display(visible=0, size=(1024, 768))
 display.start()
 driver = webdriver.Firefox()
+LINK = ['https://studio.youtube.com/video/RJWQ-xkbDt4/edit/basic',
+        'https://www.youtube.com/watch?v=3q9psT2R-Ko',
+        'https://www.youtube.com/watch?v=qkJqMGbDK40']
 while True:
-    driver.get('https://www.youtube.com/watch?v=qkJqMGbDK40')
-    time.sleep(10)
+    driver.get(LINK[0])
+    time.sleep(5)
     driver.find_element_by_class_name("ytp-play-button").click()
-    phut = random.randrange(10, 30)
+    phut = random.randrange(1, 2)
+    time.sleep(60*phut)
+
+    driver.get(LINK[1])
+    time.sleep(5)
+    driver.find_element_by_class_name("ytp-play-button").click()
+    phut = random.randrange(1, 2)
+    time.sleep(60*phut)
+
+    driver.get(LINK[2])
+    time.sleep(5)
+    driver.find_element_by_class_name("ytp-play-button").click()
+    phut = random.randrange(20, 60)
     time.sleep(60*phut)
 driver.close()
 
