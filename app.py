08dabf871f458518c04b79439709a135847b85ff
bang vu hai <bangvh@vnfintech.com>
2020-07-16 20:07:24 +0700

    add link

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from pyvirtualdisplay import Display
 import time
+import random
 
 display = Display(visible=0, size=(1024, 768))
 display.start()
 driver = webdriver.Firefox()
-driver.get('https://www.youtube.com/watch?v=Xjv1sY630Uc')
-driver.find_element_by_class_name("ytp-play-button").click()
-time.sleep(200)
+while True:
+    driver.get('https://www.youtube.com/watch?v=qkJqMGbDK40')
+    time.sleep(10)
+    driver.find_element_by_class_name("ytp-play-button").click()
+    phut = random.randrange(10, 30)
+    time.sleep(60*phut)
 driver.close()
 
 
