5a714a164c7b3ab937ee2a37d908fad5dec31929
vuhaibang <42060856+vuhaibang@users.noreply.github.com>
2020-09-02 10:43:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 display = Display(visible=0, size=(1024, 768))
 display.start()
 URL = {'https://www.youtube.com/watch?v=lH0ud6EMUoE': 60*65,
+       'https://www.youtube.com/watch?v=tixel4qz4Tc': 45*60,
       'https://www.youtube.com/watch?v=e9xj0_d-HXY': 42*60,
       'https://www.youtube.com/watch?v=1F0mYfBgID4': 18*60,
       'https://www.youtube.com/watch?v=0naW-OC1A1w': 60*60,
