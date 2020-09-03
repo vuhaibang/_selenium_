864c7b5f877f2b93dbdf17baf1e9f16fd844d4de
vuhaibang <bangvh@vnfintech.com>
2020-09-03 13:21:53 +0700

    ok

--- app.py
+++ app.py
@@ -52,8 +52,9 @@ async def run_chrome_browser():
             )
             print(f"Chrome browser run {url} in {time_sleep} s")
             body = chrome_browser.find_element_by_css_selector('body')
-            for i in range(int(time_sleep/10)):
+            for i in range(20):
                 body.send_keys(Keys.PAGE_UP)
+            for i in range(int(time_sleep/10)):
                 body.send_keys(Keys.PAGE_DOWN)
                 body.send_keys(Keys.PAGE_UP)
                 try:
